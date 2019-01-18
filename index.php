bde2373a03383fd503f0166fae265b90a1b45fca
Alejandro González Reyes <jsconestilo@gmail.com>
2019-01-17 20:34:03 -0600

    Maquetación index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="es-MX">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="description" content="Biblioteca Digital - PSEDUCA IT, es un entorno virtual que tiene por objetivo conservar en formato digital el material bibliográfico situado en la biblioteca central del Centro de Bachillerato José Vasconcelos."/>
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+		<title>Biblioteca Digital - PSEDUCA IT</title>
+		<link rel="stylesheet" href="css/general_intro.css"/>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png" />
+		<link rel="author" type="text/plain" href="humans.txt" />
+		<link rel="sitemap" type="application/xml" title="Sitemap" href="sitemap.xml">
+	</head>
+	<body>
+		<header>
+			<h1>Biblioteca Digital - PSEDUCA IT</h1>
+			<figure>
+				<img src="assets/img/libros_intro.png" alt="Logotipo Bibloteca Virtual">
+				<figcaption>Universitarios en Movimiento: Trabajo, Cultura y Progreso.</figcaption>
+			</figure>
+		</header>
+		<a href="home.php">Acceso a la Biblioteca</a>
+		<footer>
+			<address>
+				Biblioteca Digital &copy; 2013. Todos los derechos reservados.<br />
+				Centro de Bachillerato José Vasconcelos S.C.<br />
+				Actualización: 02/Dic/2013.
+			</address>
+		</footer>
+	</body>
+</html>
