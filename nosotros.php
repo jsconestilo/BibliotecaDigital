6d063f2c6e31e6f5a3b03ebfd8f77e46e0fa4fe5
Alejandro González Reyes <jsconestilo@gmail.com>
2019-01-17 20:34:55 -0600

    Maquetación nosotros.php

---
+++ nosotros.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="es-MX">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="description" content="Desarrollada en Noviembre de 2013 por Alejandro González Reyes como proyecto de Maestría, Biblioteca Digital - PSEDUCA IT tiene el firme propósito de ser una herramienta de búsqueda indispensable para toda nuestra comunidad estudiantil, profesores e investigadores."/>
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+		<title>Conoce al equipo Biblioteca Digital - PSEDUCA IT - del Centro de Bachillerato José Vasconcelos SC.</title>
+		<link rel="stylesheet" href="css/general.css"/>
+		<link rel="stylesheet" href="assets/plugins/fresco/css/fresco.css"/>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png" />
+		<link rel="author" type="text/plain" href="humans.txt" />
+		<link rel="sitemap" type="application/xml" title="Sitemap" href="sitemap.xml">
+	</head>
+	<body>
+		<div id="sesion-administrador">
+			<form name="frm_sesion" id="frm_sesion" method="post" action="" enctype="application/x-www-form-urlencoded">
+				<div>
+					<label for="txt_email_administrador">Correo / Usuario:</label>
+					<input type="text" name="txt_email_administrador" class="cambio" id="txt_email_administrador" placeholder="Tu correo / usuario">
+				</div>
+				<div>
+					<label for="txt_password_administrador">Contraseña</label>
+					<input type="password" name="txt_password_administrador" class="cambio" id="txt_password_administrador" placeholder="Tu contraseña">
+				</div>
+				<div>
+					<input type="submit" name="btn_iniciar_sesion" value="Entrar" class="cambio" id="btn_iniciar_sesion">
+				</div>
+			</form>
+		</div>
+		<div id="mostrar-sesion-administrador">
+			<span class="cambio">Iniciar Sesión</span>	
+		</div>
+		<img src="assets/img/MiniLogotipoBibliotecaDigital.png" id="logobiblioteca" alt="">
+		<nav id="menu">
+			<ul>
+				<li><a href="home.php" class="cambio">BIENVENIDOS</a></li><li><a href="libros.php" class="cambio">LIBROS</a></li><li class="logo"><a href="index.php" class="cambio"><img src="assets/img/LogotipoInstitucional.png" alt=""></a></li><li><a href="contacto.php" class="cambio">CONTACTO</a></li><li><a href="nosotros.php" class="cambio">NOSOTROS</a></li>
+			</ul>
+		</nav>
+		<div id="slideshow">
+			<div id="meteor-slideshow" class="meteor-slides navnone">
+				<div class="mslide mslide-1">
+					<img src="assets/foto/gal_000.jpg" alt="slideshow" title="slideshow">
+				</div>
+				<div class="mslide mslide-2">
+					<img src="assets/foto/gal_001.jpg" alt="slideshow" title="slideshow">
+				</div>
+				<div class="mslide mslide-3">
+					<img src="assets/foto/gal_002.jpg" alt="slideshow" title="slideshow">
+				</div>
+				<div class="mslide mslide-4">
+					<img src="assets/foto/gal_003.jpg" alt="slideshow" title="slideshow">
+				</div>
+				<div class="mslide mslide-5">
+					<img src="assets/foto/gal_004.jpg" alt="slideshow" title="slideshow">
+				</div>
+				<div class="mslide mslide-6">
+					<img src="assets/foto/gal_005.jpg" alt="slideshow" title="slideshow">
+				</div>
+				<div class="mslide mslide-7">
+					<img src="assets/foto/gal_006.jpg" alt="slideshow" title="slideshow">
+				</div>
+				<img id="slideshow_dummy" src="assets/foto/slideshow-blank.jpg" alt="" />
+			</div>
+		</div>
+		<div id="contenedor">
+			<section id="quienes_somos">
+				<h2>¿Quienes somos?</h2>
+				<p>Biblioteca Digital - PSEDUCA IT, es un entorno virtual que tiene por objetivo conservar en formato digital el material bibliográfico situado en la biblioteca central del Centro de Bachillerato José Vasconcelos. Desarrollada en Noviembre de 2013 por Alejandro González Reyes como proyecto de Maestría, Biblioteca Digital - PSEDUCA IT tiene el firme propósito de ser una herramienta de búsqueda indispensable para toda nuestra comunidad estudiantil, profesores e investigadores.</p>				
+			</section><section id="colaboradores">
+				<h2>Biblioteca Digital es posible gracias a...</h2>
+				<a href="assets/img/desarrolladores/brendan_eich.jpg" 
+				class='fresco' 
+				data-fresco-group='desarrollo' 
+				data-fresco-caption="Brendan Eich - Creador del lenguaje de Programación JavaScript">
+					<img src="assets/img/desarrolladores/brendan_eich.jpg">
+					<figcaption>Brendan Eich - JavaScript</figcaption>
+				</a><a href="assets/img/desarrolladores/hakon_lie.jpg" 
+				class='fresco' 
+				data-fresco-group='desarrollo' 
+				data-fresco-caption="Hakon Lie - Creador del lenguaje de Hojas de Estilo CSS">
+					<img src="assets/img/desarrolladores/hakon_lie.jpg">
+					<figcaption>Hakon Lie - CSS</figcaption>
+				</a><a href="assets/img/desarrolladores/john_resing.jpg" 
+				class='fresco' 
+				data-fresco-group='desarrollo' 
+				data-fresco-caption="John Resing - Creador del Framework JavaScript jQuery">
+					<img src="assets/img/desarrolladores/john_resing.jpg">
+					<figcaption>John Resing - jQuery</figcaption>
+				</a><a href="assets/img/desarrolladores/michael_widenius.jpg" 
+				class='fresco' 
+				data-fresco-group='desarrollo' 
+				data-fresco-caption="Michael Widenius - Creador del Gestor de Bases de Datos MySQL">
+					<img src="assets/img/desarrolladores/michael_widenius.jpg">
+					<figcaption>Michael Widenius - MySQL</figcaption>
+				</a><a href="assets/img/desarrolladores/rasmus_lerdorf.jpg" 
+				class='fresco' 
+				data-fresco-group='desarrollo' 
+				data-fresco-caption="Rasmus Lerdorf - Creador del lenguaje de Programación PHP">
+					<img src="assets/img/desarrolladores/rasmus_lerdorf.jpg">
+					<figcaption>Rasmus Lerdorf - PHP</figcaption>
+				</a><a href="assets/img/desarrolladores/tim_berners.jpg" 
+				class='fresco' 
+				data-fresco-group='desarrollo' 
+				data-fresco-caption="Tim Berners - Creador del lenguaje de Marcado de Hipertexto HTML">
+					<img src="assets/img/desarrolladores/tim_berners.jpg">
+					<figcaption>Tim Berners - HTML</figcaption>
+				</a>
+			</section>
+		</div>
+		<footer id="pie">
+			<section>
+				<h3>Sobre nosotros</h3>
+				<p>Biblioteca Digital es parte PSEDUCA IT, un entorno virtual que tiene por objetivo ser un punto de encuentro entre alumnos de la comunidad estudiantil y profesores.</p>
+				<img src="assets/img/MiniLogotipoPSEDUCA.png" alt="">
+			</section><section>
+				<h3>Newsletter</h3>
+				<p>Registrese y reciba mensualmente de forma gratuita nuestros boletines informativos con las últimas noticias acerca de cursos y diplomados.</p>
+				<form name="frm_boletines" id="frm_boletines" action="" method="post" enctype="application/x-www-form-urlencoded">
+					<input type="email" name="txt_email_boletin" id="txt_email_boletin" placeholder="Correo Electrónico">
+					<input type="submit" name="btn_suscribirse" id="btn_suscribirse" class="cambio" value="Registrar">
+				</form>
+			</section><section>
+				<h3>Estamos en las redes</h3>
+				<p>Nos complace comunicar a nuestros alumnos y profesores que a partir de este momento ya nos pueden visitar a través de las redes sociales.</p>
+				<a href="" class="facebook"><span>Facebook PrepaJV</span></a>
+				<a href="" class="feed"><span>Feed PrepaJV</span></a>
+				<a href="" class="youtube"><span>Youtube PrepaJV</span></a>
+				<a href="" class="twitter"><span>Twitter PrepaJV</span></a>
+			</section>
+		</footer>
+		<div class="flecha">
+			<img src="assets/img/flecha.png" alt="">
+		</div>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script>
+			(!window.jQuery) && document.write("<script src='js/jquery183.min.js'><\/script>");
+		</script>
+		<script src="assets/plugins/meteor-slideshow/jquery.cycle.lite.min.js"></script>
+		<script src="assets/plugins/meteor-slideshow/jquery.metadata.js"></script>
+		<script src="js/general.js"></script>
+		<script src="assets/plugins/meteor-slideshow/slideshow.js"></script>
+		<script src="assets/plugins/fresco/js/fresco.js"></script>
+	</body>
+</html>
